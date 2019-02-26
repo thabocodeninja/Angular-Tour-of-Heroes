6e65052d24356e442dabd127cd0a4e7cf1607522
thabo <thaboledwaba@theditalacademy.co.za>
2019-02-26 17:58:08 +0200

    Add Hero Component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heroes');
+    expect(app.title).toEqual('');
   });
 
   it('should render title in a h1 tag', () => {
